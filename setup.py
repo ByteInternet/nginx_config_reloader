ee4a80d56c79ed749b33ad85f72eae3f317925fb
Rick van de Loo <rickvandeloo@gmail.com>
2018-12-24 11:13:16 +0100

    Update version in setup.py to 20181224.111316

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nginx_config_reloader',
-    version="20181224.111310",
+    version="20181224.111316",
     packages=find_packages(exclude=['test*']),
     url='',
     license='',
