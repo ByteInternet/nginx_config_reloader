862cc76ae672e550f432146404e007d138c28f68
Staging Jenkins <jenkins@hypernode.com>
2022-03-17 09:36:32 +0000

    [automated] Marking Release 20220317.093631

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nginx_config_reloader',
-    version="20211014.163011",
+    version="20220317.093631",
     packages=find_packages(exclude=['test*']),
     url='https://github.com/ByteInternet/nginx_config_reloader',
     license='',
